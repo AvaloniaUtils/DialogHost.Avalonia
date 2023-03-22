eb2b761e1e81775ad20d4e2ee72573413117c4d8
SKProCH <skproch@yandex.ru>
2023-03-23 01:43:41 +0300

    Make positioners work properly

--- DialogHost.Avalonia/DialogControls/PositionedContentControl.cs
+++ DialogHost.Avalonia/DialogControls/PositionedContentControl.cs
@@ -6,29 +6,47 @@ using DialogHostAvalonia.Positioners;
 
 namespace DialogHostAvalonia.DialogControls {
     public class PositionedContentControl : ContentControl {
-        public static readonly StyledProperty<IDialogPopupPositioner> PositionerProperty = 
-            AvaloniaProperty.Register<PositionedContentControl, IDialogPopupPositioner>(nameof(Positioner));
+        static PositionedContentControl() {
+            AffectsArrange<PositionedContentControl>(PositionerProperty);
+            PositionerProperty.Changed.AddClassHandler<PositionedContentControl>((control, args) => {
+                var (oldValue, newValue) = args.GetOldAndNewValue<IDialogPopupPositioner?>();
+                if (oldValue != null) {
+                    oldValue.RearrangeRequested -= control.OnRearrangeRequested;
+                }
+                if (newValue != null) {
+                    newValue.RearrangeRequested += control.OnRearrangeRequested;
+                }
+            });
+        }
 
-        public IDialogPopupPositioner Positioner {
-            get { return GetValue(PositionerProperty); }
-            set { SetValue(PositionerProperty, value); }
+        public static readonly StyledProperty<IDialogPopupPositioner?> PositionerProperty = 
+            AvaloniaProperty.Register<PositionedContentControl, IDialogPopupPositioner?>(nameof(Positioner));
+
+        public IDialogPopupPositioner? Positioner {
+            get => GetValue(PositionerProperty);
+            set => SetValue(PositionerProperty, value);
+        }
+        
+        private void OnRearrangeRequested(object sender, EventArgs e) {
+            InvalidateArrange();
         }
 
         /// <inheritdoc />
         protected override void ArrangeCore(Rect finalRect) {
             var margin = Margin;
 
-            var useLayoutRounding = UseLayoutRounding;
-            var scale = LayoutHelper.GetLayoutScale(this);
-            if (useLayoutRounding)
-            {
-                margin = LayoutHelper.RoundLayoutThickness(margin, scale, scale);
-            }
-
-            var availableSizeMinusMargins = new Size(
+            var size = new Size(
                 Math.Max(0, finalRect.Width - margin.Left - margin.Right),
                 Math.Max(0, finalRect.Height - margin.Top - margin.Bottom));
-            Positioner.Arrange(DesiredSize, availableSizeMinusMargins, scale);
+            
+            var contentSize = new Size(
+                Math.Min(size.Width, DesiredSize.Width - margin.Left - margin.Right), 
+                Math.Min(size.Height, DesiredSize.Height - margin.Top - margin.Bottom));
+            var positioner = Positioner ?? CenteredDialogPopupPositioner.Instance;
+            var bounds = positioner.Arrange(contentSize, size, 1);
+            
+            ArrangeOverride(bounds.Size).Constrain(size);
+            Bounds = new Rect(bounds.X + margin.Left, bounds.Y + margin.Top, bounds.Width, bounds.Height);
         }
     }
 }
